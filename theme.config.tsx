1ad5942b469e625827fecb92543411f824f44b1a
Ryder Selikow <50812202+Rydersel@users.noreply.github.com>
2024-09-18 21:47:47 -0700

    Add files via upload

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>EDUrange Cloud</span>,
+  project: {
+    link: 'https://github.com/Rydersel/EDUrange_Cloud',
+  },
+  chat: {
+    link: 'https://discord.com',
+  },
+  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  footer: {
+    text: 'EDURange Cloud Docs',
+  },
+}
+
+export default config
